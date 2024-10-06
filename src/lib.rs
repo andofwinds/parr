f9217136542ca2edc65b3be02e7a0d8ebbb4020c
andofwinds <=>
2024-10-06 22:46:56 +0300

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+#![no_std]
+//! # The **P**ointer **Arr**ay.
+//!
+//! ## Turn your pointer into array!
+//!
+//! Provides a C-like unknown-length array from pointer. (The `T[]` type).
+//!
+//! # Examples.
+//! ## Simple `[1; 2; 3]` array from slice.
+//! Rust:
+//! ```rust
+//! let arr = Parr::<u8>::from([1, 2, 3].as_slice());
+//! assert_eq!(arr[1], 2);
+//! ```
+//! C equivalent:
+//! ```C
+//! uint8_t arr[] = {1, 2, 3};
+//! ```
+//!
+//! ## From pointer.
+//! ```rust
+//! let arr = Parr::<u8>::new([1, 2, 3].as_ptr() as u64);
+//! assert_eq!(arr[1], 2);
+//! ```
+
+use core::ops::Index;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(transparent)]
+/// # The Pointer Array.
+///
+/// Holds pointer to a first element of the array (base address).
+///
+/// # Generics:
+/// - `T`: Type of each element in the array.
+pub struct Parr<T: Sized> ( *mut T );
+impl<T: Sized> Parr<T> {
+    /// ## Creates new array from the given `base` address pointer.
+    ///
+    /// ## Safety
+    /// This function is safe but you need to make sure that pointer address is right.
+    ///
+    /// ## Generics:
+    /// - `B`: Type of the `base` argument, anything that can be casted to `u64`.
+    pub fn new<B: Into<u64>>(base: B) -> Self {
+        Self ( base.into() as *mut T )
+    }
+}
+impl<T: Sized> From<&[T]> for Parr<T> {
+    fn from(value: &[T]) -> Self {
+        Self (
+            value
+                    as *const _
+                    as *const u64
+                    as u64
+                    as *mut T
+        )
+    }
+}
+impl<T: Sized> Index<usize> for Parr<T> {
+    type Output = T;
+    fn index(&self, index: usize) -> &Self::Output {
+        unsafe {
+            &*self.0.add(index)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Parr;
+
+    #[test]
+    fn from_slice() {
+        let arr: Parr<u8> = Parr::from([11_u8, 22, 33].as_slice());
+
+        assert_eq!(arr[1], 22);
+    }
+
+    #[test]
+    fn from_ptr() {
+        let arr: Parr<u8> = Parr::new([11_u8, 22, 33].as_ptr() as u64);
+
+        assert_eq!(arr[1], 22);
+    }
+}
